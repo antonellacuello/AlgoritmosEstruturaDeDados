321164383ea0389d897f17216a19307e9a7cf9ed
Antonella Cuello <122099426+antonellacuello@users.noreply.github.com>
2024-07-12 09:13:10 -0300

    Create 12-07-24.py
    Exercícios da aula 12/07/2024

---
+++ aed1/12-07-24.py
@@ -0,0 +1,5 @@
+# Exercícios da aula 12/07/2024
+
+# 1. Leia o nome do usuário e escreva uma mensagem de boas-vindas com seu nome.
+'''username = input('Insira seu nome de usuário: ')
+print(f'Seja bem-vindo {username}!')''
