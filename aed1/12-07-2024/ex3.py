8e22fbb0473d047dc08a8f6918960bb28feeb937
Antonella Cuello <122099426+antonellacuello@users.noreply.github.com>
2024-07-12 09:31:19 -0300

    Create ex3.py

---
+++ aed1/12-07-2024/ex3.py
@@ -0,0 +1,6 @@
+# Exercícios da aula 12/07/2024
+                                                 
+# 3. Leia o custo do produto em dólar e escreva o valor em reais.
+d = 5.7
+cp = float(input('Digite o custo do produto em dólar: '))
+print(f'O custo do produto em reais será de {cp * d}!')
