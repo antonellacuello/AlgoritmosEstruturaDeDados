30ba2dece43c4dd1d2590369b25cca29b60533bf
Antonella Cuello <122099426+antonellacuello@users.noreply.github.com>
2024-07-12 09:21:14 -0300

    Create ex2.py

---
+++ aed1/12-07-2024/ex2.py
@@ -0,0 +1,12 @@
+# Exercícios da aula 12/07/2024
+                                                 
+# 2. Leia a distância percorrida e o tempo gasto. Escreva a velocidade.
+print('----------O íncrivel medidor de velocidade----------')
+print('----------------------------------------------------')
+print('                                                    ')
+
+dp = float(input('Insira a distância percorrida, em km: '))
+t = float(input('Agora, insira o tempo gasto, em horas: '))
+v = dp / t
+
+print(f'Sua velocidade foi de {v} km/h.')
