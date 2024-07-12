8281651b7f1f74cb56aba626aed6ca2f07a964ad
Antonella Cuello <122099426+antonellacuello@users.noreply.github.com>
2024-07-12 10:15:18 -0300

    Update ex4.py

--- aed1/12-07-2024/ex4.py
+++ aed1/12-07-2024/ex4.py
@@ -2,7 +2,12 @@
 
 # 4. Leia a temperatura em graus Celsius e converta para Fahrenheit.
 
-c = float(input('Insira a temperatura em °C: '))
-f = (c * 9/5) + 32
-
-print(f'A temperatura em Fahrenheit é {f}°F!')
+cf = input('Você deseja converter a temperatura de C para F? Responda com SIM ou NAO ')
+if cf.lower() in 'sim':
+	c = float(input('Insira a temperatura em °C: '))
+	f = (c * 9/5) + 32
+	print(f'A temperatura em Fahrenheit é {f}°F!')
+else:
+	f = float(input('Insira a temperatura em °F: '))
+	c = (f - 32) * 5 / 9
+	print(f'A temperatura em Celsius é {c}°C!')
