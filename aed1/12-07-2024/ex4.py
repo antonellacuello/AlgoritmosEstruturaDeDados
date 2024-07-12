252d8296ff51a97477dc874cad42ca4f45267787
Antonella Cuello <122099426+antonellacuello@users.noreply.github.com>
2024-07-12 09:57:08 -0300

    Create ex4.py

---
+++ aed1/12-07-2024/ex4.py
@@ -0,0 +1,6 @@
+# 4. Leia a temperatura em graus Celsius e converta para Fahrenheit.
+
+c = float(input('Insira a temperatura em °C: '))
+f = (c * 9/5) + 32
+
+print(f'A temperatura em Fahrenheit é {f}°F!')
