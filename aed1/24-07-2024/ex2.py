7962e6e4c4c768adbaae6ca676af61449cf97a50
Antonella Cuello <122099426+antonellacuello@users.noreply.github.com>
2024-07-24 10:27:17 -0300

    Create ex2.py

---
+++ aed1/24-07-2024/ex2.py
@@ -0,0 +1,8 @@
+# 2. Leia um número e diga se ele é par ou ímpar.
+
+n = int(input('Digite um número: '))
+
+if n % 2 == 0:
+	print('O número escolhido é par!')
+else:
+	print('O número escolhido é ímpar!')
