fd99106017f19b4b437fb2fb4feaab83f0932fb9
Antonella Cuello <122099426+antonellacuello@users.noreply.github.com>
2024-07-26 08:38:25 -0300

    Create senha.py

---
+++ aed1/26-07-2024/senha.py
@@ -0,0 +1,8 @@
+# Verificação de senha
+
+senha = input('Para acessar, digite a senha: ')
+
+while senha != 'aed1':
+	print('Senha incorreta. Tente novamente!')
+	senha = input('Digite a senha: ')
+print('Senha correta. Seja bem-vindo ao sistema!')
