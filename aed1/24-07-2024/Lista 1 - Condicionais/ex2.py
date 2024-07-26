e26e88eb61ec79261967018aa6baf2f694659429
Antonella Cuello <122099426+antonellacuello@users.noreply.github.com>
2024-07-26 08:42:33 -0300

    Delete aed1/24-07-2024/Lista 1 - Condicionais directory

--- aed1/24-07-2024/Lista 1 - Condicionais/ex2.py
+++
@@ -1,8 +0,0 @@
-# 2. Leia um número e diga se ele é par ou ímpar.
-
-n = int(input('Digite um número: '))
-
-if n % 2 == 0:
-	print('O número escolhido é par!')
-else:
-	print('O número escolhido é ímpar!')
