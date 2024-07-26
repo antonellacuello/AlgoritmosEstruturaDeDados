3ae0c48a68b40610ca7a08a691c2165d8d92e4ad
Antonella Cuello <122099426+antonellacuello@users.noreply.github.com>
2024-07-26 09:31:16 -0300

    Update senha.py

--- aed1/26-07-2024/senha.py
+++ aed1/26-07-2024/senha.py
@@ -1,19 +1,10 @@
-# Verificação de senha
-
-senha = input('Para acessar, digite a senha: ')
-
-while senha != 'aed1':
-	print('Senha incorreta. Tente novamente!')
-	senha = input('Digite a senha: ')
-print('Senha correta. Seja bem-vindo ao sistema!')
-
 # Verificação de senha com limite de tentativas
 
 senha = 'aed1'
 c = 0
 tentativa = input('Digite a senha: ')
 
-while c != 3:
+while c != 5:
 	c = c + 1
 	if tentativa != senha:
 		print('Senha incorreta. Tente novamente! \n')
@@ -21,3 +12,5 @@ while c != 3:
 
 if tentativa == senha:
 	print('Senha correta. Seja bem-vindo ao sistema!')
+else:
+	print('Limite de tentativas atingido. Desista.')
