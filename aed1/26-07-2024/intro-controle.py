a2a232f0a0a0029faa23c8233e078b7dc3f8b7ad
Antonella Cuello <122099426+antonellacuello@users.noreply.github.com>
2024-07-26 08:57:00 -0300

    Create intro-controle.py

---
+++ aed1/26-07-2024/intro-controle.py
@@ -0,0 +1,19 @@
+# Introdução a repetição e os controles
+
+'''print('Começou!')
+c = 0
+
+while 3 < 5:
+	print(c, ' Eu odeio o prisco...')
+	c =+ 1
+
+print('FIM')''' # Loop infinito
+
+print('Começou!')
+c = 0
+
+while c <= 4:
+	print(c, ' Eu odeio o prisco...')
+	c += 1
+
+print('No fundo, eu amo') # Loop com fim
