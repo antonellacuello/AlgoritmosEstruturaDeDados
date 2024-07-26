7260d1fee0ec6804202b683624fc2f2aade0cd7c
Antonella Cuello <122099426+antonellacuello@users.noreply.github.com>
2024-07-26 10:03:23 -0300

    Create ex6.py

---
+++ aed1/24-07-2024/Lista 2 - Condicionais/ex6.py
@@ -0,0 +1,11 @@
+# 6. Faça um programa em python que leia 3 números e os mostre em ordem crescente.
+
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite um outro número: '))
+n3 = float(input('E, por, digite o último número: '))
+
+if n1 > n2 and n1 > n3:
+	p = n1
+else:
+	if n2 > n1 and n2 > n3:
+	
