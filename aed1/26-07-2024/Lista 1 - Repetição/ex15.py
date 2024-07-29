774d32bcb87296249aa523fbaac8ebb0ce5dde4d
Antonella Cuello <122099426+antonellacuello@users.noreply.github.com>
2024-07-29 19:49:57 -0300

    Create ex15.py

---
+++ aed1/26-07-2024/Lista 1 - Repetição/ex15.py
@@ -0,0 +1,24 @@
+#15. Escreva um programa que receba um número inteiro positivo do usuário e verifique se ele é primo.
+
+n = int(input('Digite um número inteiro e positivo: '))
+
+while n < 0:
+  n = int(input('Digite um número INTEIRO e POSITIVO: '))
+
+if n <= 1:
+    print(f'{n} não é um número primo.')
+else:
+    primo = True
+    divisor = 2
+    
+    while divisor * divisor <= n:
+        if n % divisor == 0:
+            primo = False
+            break
+        divisor += 1
+    
+
+    if primo == True:
+        print(f'{n} é um número primo.')
+    else:
+        print(f'{n} não é um número primo.')
