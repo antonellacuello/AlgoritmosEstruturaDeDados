fbb79fc40443f5e6e2153602067b53f415c0061f
Antonella Cuello <122099426+antonellacuello@users.noreply.github.com>
2024-07-31 11:43:41 -0300

    Create piramide.py

---
+++ aed1/31-07-2024/piramide.py
@@ -0,0 +1,20 @@
+# Escreva um programa que mostre a seguinte sequência, tendo informado sua altura e elemento a ser repetido
+'''
+
+           1
+         2 2 2
+       3 3 3 3 3
+     4 4 4 4 4 4 4
+   5 5 5 5 5 5 5 5 5
+   …
+   N N N N N N N …
+   
+'''
+altura = int(input('Informe a altura da escadinha'))
+elemento = input('Do que a escadinha será feita?')
+c = 1
+while 
+	while c <= altura:
+		print(c * tijolo)
+		c += 1
+# a ser terminado
