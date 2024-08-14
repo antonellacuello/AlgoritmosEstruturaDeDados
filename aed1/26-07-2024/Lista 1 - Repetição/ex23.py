ddd93bf1eb6b52fc313a9ce02645a0b4a43cf4eb
Antonella Cuello <122099426+antonellacuello@users.noreply.github.com>
2024-08-14 10:33:19 -0300

    Update ex23.py

--- aed1/26-07-2024/Lista 1 - Repetição/ex23.py
+++ aed1/26-07-2024/Lista 1 - Repetição/ex23.py
@@ -1,3 +1,6 @@
+# Escreva um programa que leia dois valores x e y. Em seguida escreva quais são os números primos contidos neste intervalo. Por exemplo, para x=3 
+# e y=14 escreva: 3,5,7,11,13. Verifique se o usuário digitou x e y de modo que x<y.
+
 inicio = 3
 fim = 14
 num = inicio
