4912b16d73d48a11ac0d9f743c42658fb5639c20
Antonella Cuello <122099426+antonellacuello@users.noreply.github.com>
2024-09-06 10:25:19 -0300

    Introdução aos controles e repetições.

--- aed1/07-26-2024/intro-controle.py
+++ aed1/07-26-2024/intro-controle.py
@@ -7,7 +7,7 @@ while 3 < 5:
 	print(c, ' Eu odeio o prisco...')
 	c =+ 1
 
-print('FIM')''' # Loop infinito
+print('FIM')'''  # Loop infinito
 
 print('Começou!')
 c = 0
@@ -16,4 +16,4 @@ while c <= 4:
 	print(c, ' Eu odeio o prisco...')
 	c += 1
 
-print('No fundo, eu amo') # Loop com fim
+print('No fundo, eu amo')  # Loop com fim
