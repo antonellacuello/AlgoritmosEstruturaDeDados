628cc9a79700b5aec5954d3388fc410aa2a209a3
Antonella Cuello <122099426+antonellacuello@users.noreply.github.com>
2024-09-06 10:22:29 -0300

    Iniciando os conhecimentos em Python.

--- aed1/07-10-2024/helloworld.py
+++ aed1/07-10-2024/helloworld.py
@@ -1,10 +1,10 @@
-print('Alô mundo dos Algoritmos!');
-print('Eu amo o Prisco \n' * 10);
-E = 7;
-print('E');
-print(E);
-E = 'e agora?';
-print(type(E));
-print(E);
-nome = input('Digite seu nome: ');
-print('Konnichiwa, ' + nome);
+print('Alô mundo dos Algoritmos!')
+print('Eu amo o Prisco \n' * 10)
+E = 7
+print('E')
+print(E)
+E = 'e agora?'
+print(type(E))
+print(E)
+nome = input('Digite seu nome: ')
+print('Konnichiwa, ' + nome)
