439b85a800958f03db35f4197fbc7069b83758e3
Antonella Cuello <122099426+antonellacuello@users.noreply.github.com>
2024-12-06 13:15:12 +0000

    Aula de Exercícios de Arquivos

--- aed1/11-22-2024/intro-arquivos.py
+++ aed1/11-22-2024/intro-arquivos.py
@@ -1,4 +1,4 @@
-# AULA DE ARQUIVOS - 22/11/2024
+# AULA DE ARQUIVOS - 22/11/2024 e 06/12/2024
 
 arq = open('poema.txt', 'r')
 #arq - ponteiro para o arquivo
@@ -9,6 +9,17 @@ print(texto)
 print('---------------')
 print()'''
 
-lista = arq.readlines()
-print(lista)
-arq.close()
+print(arq)  # fala as informações do arquivo 
+#lista = arq.readlines()   #tranforma em uma lista de linhas
+#print(lista)
+'''linha = arq.readline()
+print(linha)'''
+
+segue = True 
+while segue:
+    linha = arq.readline()
+    print(linha)
+    if linha == '':
+        segue = False
+
+arq.close()   #serve para fechar o arquivo e liberar memória
