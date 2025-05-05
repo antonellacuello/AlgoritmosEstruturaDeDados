2e8e3d3d2e047ad9c29977e4d81e011e2b99d894
Antonella Cuello <122099426+antonellacuello@users.noreply.github.com>
2025-05-05 17:24:28 +0000

    Exercício de Ordenação por Seleção

---
+++ aed2/Rodrigo/crescer.py
@@ -0,0 +1,14 @@
+vetor = [3, 4, 2, 1]
+tamanho = len(vetor)
+crescido = []
+aux = vetor[0]
+i = 0
+
+while i <= tamanho:
+    numero = vetor[i]
+    if numero > aux:
+        crescido[0] = numero
+        a
+    else:
+
+print(vetor)
